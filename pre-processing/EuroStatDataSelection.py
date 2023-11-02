6bd9720a55fd1de9f09e8e001c2420a9a4a3e116
BachirNILU <117657891+BachirNILU@users.noreply.github.com>
2023-11-02 10:26:51 +0100

    Update EuroStatDataSelection.py

--- pre-processing/EuroStatDataSelection.py
+++ pre-processing/EuroStatDataSelection.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy
 import collections
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     city_cube = 'C_urban_cube_sh.sqlite'
     # get Eurostat number of population data
@@ -39,14 +38,19 @@ if __name__ == '__main__':
                 y = y + ([str(k)]*len(lcities))
                 for j in lcities:
                     for i in [target] + features:
-                        indic[i].append(list(inter[(inter['urau_code']==j)& (inter['indic_code']==i)][str(k)])[0])
-
+                        if(len(list(inter[(inter['urau_code']==j)& (inter['indic_code']==i)][str(k)]))>0):
+                            indic[i].append(list(inter[(inter['urau_code']==j)& (inter['indic_code']==i)][str(k)])[0])
+                        else:
+                            indic[i].append(None)
     d['City'] = cities
     d['Year'] = y
     for i in [target] + features:
         d[i] = indic[i]
 
     df = pd.DataFrame(data=d)
+    for i in [target] + features:
+        df = df[~df[i].isna()]
+    print('Train/Test data size: ', len(df))
     df.to_csv('SelectedData_' + target+ '.csv', index=False)
 
     d2 = {}
@@ -75,7 +79,6 @@ if __name__ == '__main__':
             if mask:
                 missing = missing.append(dict(zip(missing.columns, [j, y, target])), ignore_index=True)
 
+    print('Missing data size: ', len(missing))
     missing.to_csv('Missing_' + target+ '.csv', index=False)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
